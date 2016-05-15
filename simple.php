0b9e814d556babac80e3520b72da4855a4aa15fd
Teerasej Jiraphatchandej <teerasej@gmail.com>
2016-05-15 12:36:49 +0700

    Rename original index.php to simple.php to be used in testing web api

---
+++ simple.php
@@ -0,0 +1,56 @@
+<?php
+require 'vendor/autoload.php';
+
+$app = new \Slim\App;
+$container = $app->getContainer();
+
+// Add monolog to container as a service
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+
+// Define CorsSlim for allow cross-platform access
+// $corsOptions = array(
+//     "origin" => "*",
+//     "allowMethods" => array("POST, GET")
+//     );
+// $cors = new \CorsSlim\CorsSlim($corsOptions);
+// $app->add($cors);
+
+
+// Below is sample routing
+
+$app->get('/', function ( $request,  $response) {
+	$response->write("Hi, This is Pon!");
+	return $response;
+});
+
+$app->get('/hello/{name}', function ( $request,  $response) {
+    $name = $request->getAttribute('name');
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+
+$app->post('/', function($request, $response){
+  $response->getBody()->write("POST!");
+  return $response;
+});
+
+$app->post('/sign-in', function($request, $response){
+
+  $data = $request->getParsedBody();
+
+  // $this->logger->addInfo('POST data: ' . var_dump($data));
+  $response->getBody()->write($data['username']);
+  return $response;
+});
+
+
+$app->run();
+?>
